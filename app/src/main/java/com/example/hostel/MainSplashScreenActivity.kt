374b7afe0923cf7e2033062543b8e888130fac40
nilesh.ingale <nilesh.ingale@springct.com>
2020-02-19 15:17:03 +0530

    test commit

---
+++ app/src/main/java/com/example/hostel/MainSplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.hostel
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_mainScr_login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_mainScr_register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
